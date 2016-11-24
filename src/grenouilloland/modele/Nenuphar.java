156dabf9e922f64f996afaa211b584d53fed59f5
guiguy <guiguyz@hotmail.fr>
2016-11-24 22:06:00 +0100

    JavaDoc Nenuphar.

--- src/grenouilloland/modele/Nenuphar.java
+++ src/grenouilloland/modele/Nenuphar.java
@@ -10,14 +10,20 @@ package grenouilloland.modele;
 public class Nenuphar {
 
 
+    /**
+     * Constructeur logique.
+     *
+     * @param type le type de nenuphar
+     */
     public Nenuphar(TypeElement type){
         this.type=type;
         this.age=age.GRAND;
     }
 
     /**
-     * Constructeur par defaut
-     * Surcharge de constructeur
+     *
+     * Constructeur par defaut.
+     *
      * definnisant un nenuphar de base
      * comme étant de l'eau
      * et comme etant toujours grand.
@@ -30,6 +36,13 @@ public class Nenuphar {
 
 
 
+    /**
+     * vieillir un nenuphar.
+     *
+     * vieillis tout type de nenuphar excepté l'eau
+     * et les nenuphar immortel
+     *
+     */
     public void vieillir(){
             if(getAge()==age.GRAND){
                 setAge(age.MOYEN);
@@ -44,22 +57,52 @@ public class Nenuphar {
 
 
 
+    /**
+     * Accesseur.
+     *
+     * @return retourne le type d'un nenuphar.
+     */
     public TypeElement getType() {
         return type;
     }
 
+    /**
+     * Accesseur.
+     *
+     * @return retourne l'age d'un nenuphar.
+     */
     public Age getAge() {
         return age;
     }
 
+    /**
+     * Mutateur.
+     *
+     * @param age modifie l'age d'un nenuphar.
+     */
     public void setAge(Age age) {
         this.age = age;
     }
 
+
+    /**
+     * Mutateur.
+     *
+     * @param type modifie le type d'un nenuphar.
+     */
     public void setType(TypeElement type) {
         this.type = type;
     }
 
+    /**
+     * type d'un nenuphar.
+     *
+     */
     protected TypeElement type;
+
+    /**
+     * age d'un nenuphar.
+     *
+     */
     protected Age age;
 }
