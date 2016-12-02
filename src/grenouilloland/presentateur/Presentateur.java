6daa0fcdbd7bf44afe1739202183058c0639f89a
21009341 <guiguyz@hotmail.fr>
2016-12-02 16:36:24 +0100

    Javadoc

--- src/grenouilloland/presentateur/Presentateur.java
+++ src/grenouilloland/presentateur/Presentateur.java
@@ -47,7 +47,7 @@ public class Presentateur {
     }
 
     /**
-     * Retourne la resolution du modele.
+     * Resolution du modele.
      *
      * @return la resolution du modele.
      */
@@ -69,46 +69,77 @@ public class Presentateur {
     }
 
     /**
-     * Retourne le contenu de la case dont la position est fournie en argument.
+     * Type d'élément dans la case dont la position est fournie en argument.
      *
      * @param position la position.
-     * @return le contenu de la case correspondante.
+     * @return le Type d'élément contenu dans la case correspondante.
      */
     public TypeElement lireType(Position position) {
         return modele.lireType(position);
     }
 
+    /**
+     * Accesseur sur le nenuphar en fonction de la position.
+     *
+     * @return la valeur position de {@link Modele#getNenuphar(Position)}.
+     */
     public Nenuphar getNenuphar(Position position) {
         return modele.getNenuphar(position);
     }
 
+    /**
+     * Genere le chemin du modele au lancement de partie.
+     */
     public void lancerPartie() {
         modele.genereChemin();
     }
 
 
+
+    /**
+     * Viellit les nenuphars.
+     * Genere le chemin du modele durant son évolution.
+     */
     public void vieillirNenuphar() {
         modele.vieillirNenuphar();
         modele.genereChemin();
     }
 
+    /**
+     * Accesseur sur la grenouille.
+     *
+     * @return la grenouille {@link Modele#getGrenouille()}.
+     */
     public Grenouille getGrenouille() {
         return modele.getGrenouille();
     }
 
+    /**
+     * Déplace la grenouille.
+     * @param position la nouvelle position de la grenouille
+     */
     public void deplacerGrenouille(Position position) {
         modele.deplacerGrenouille(position);
         genererChemin();
     }
 
+    /**
+     * Genere le chemin du modele.
+     */
     public void genererChemin(){
         modele.genereChemin();
     }
 
+    /**
+     * Genere le chemin du modele.
+     */
     public boolean gagnant() {
         return modele.gagnant();
     }
 
+    /**
+     * Genere le chemin du modele.
+     */
     public boolean perdant() { return modele.perdant(); }
 
 
